d632435584ad43c3308e76f39ae7f5c42fff0d33
johanOA <johana.ospinao@uqvirtual.edu.co>
2024-03-06 11:11:56 -0500

    Aplicacion funcionando 1.1

--- users/src/main/java/com/example/users/models/UsuarioModel.java
+++ users/src/main/java/com/example/users/models/UsuarioModel.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import jakarta.persistence.*;
 import lombok.*;
 
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -16,27 +17,29 @@ public class UsuarioModel {
     @Id
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, name = "id")
     private Integer id;
 
     @NonNull
-    @Column(nullable = false, length = 12)
+    @Column(nullable = false, length = 12, name = "first_Name")
     private String firstName;
 
     @NonNull
-    @Column(nullable = false, length = 12)
+    @Column(nullable = false, length = 12, name = "last_Name")
     private String lastName;
     
     @NonNull
-    @Column(nullable = false)
+    @Column(nullable = false, name = "email")
     private String email;
     
     @NonNull
-    @Column(nullable = false, length = 12)
+    @Column(nullable = false, length = 12, name = "contrasena")
     private String contrasena;  
 
+    @Column(name = "created_at")
     private LocalDateTime created_at;
 
+    @Column(name = "updated_at")
     private LocalDateTime updated_at;
     
 }
--- users/src/main/java/com/example/users/repositories/UsuarioRepository.java
+++ users/src/main/java/com/example/users/repositories/UsuarioRepository.java
@@ -1,15 +1,18 @@
 package com.example.users.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.users.models.UsuarioModel;
 
 @Repository
-public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer>{
-    // Método para obtener un usuario por ID
-    UsuarioModel findById(long id);
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer>{
+   
+    @Query("select u from UsuarioModel u")
+    Page<UsuarioModel> listarUsuarios(Pageable pageable);
 
-    // Método para actualizar un usuario
-    UsuarioModel save(UsuarioModel usuario);
-} 
+    
+}
